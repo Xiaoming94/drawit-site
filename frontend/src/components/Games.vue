67deb47681bad615bdfa1dbfd389642bf00a919c
Henry Yang <mail@xiaoming.se>
2018-06-25 18:42:00 +0200

    Add initial component for displaying games

---
+++ frontend/src/components/Games.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="games">
+    <h1> Games </h1>
+    <ul>
+        <li v-for="game in games" :key="game.id">
+            <h2>{{ game.name }}</h2>
+            <p>
+              {{ game.description }}
+            </p>
+        </li>
+        <p>
+          Loop exited without problemm
+        </p>
+    </ul>
+  </div>
+</template>$
+<script>
+export default {
+  name: 'Games',
+  data () {
+    return {
+      games: []
+    }
+  },
+
+  created () {
+    fetch('http://localhost:8080/json/games')
+      .then(response => { return response.json() })
+      .then(json => { this.games = json })
+  }
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Games from '@/components/Games'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/games',
+      name: 'Games',
+      component: Games
     }
   ]
 })
