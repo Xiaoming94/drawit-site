96c0e87a0b484dfd38e970cd79de11f21fd3923f
Henry Yang <mail@xiaoming.se>
2018-06-25 18:41:37 +0200

    Add CrossOrigin Request allowance

--- backend/src/main/kotlin/org/drawit/backend/games/GamesController.kt
+++ backend/src/main/kotlin/org/drawit/backend/games/GamesController.kt
@@ -1,12 +1,13 @@
 package org.drawit.backend.games
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-
+@CrossOrigin(origins = ["http://localhost:8081"])
 @RequestMapping("/json/games")
 
 class GamesController(@Autowired private val gameReposiory : GameRepository ) {
