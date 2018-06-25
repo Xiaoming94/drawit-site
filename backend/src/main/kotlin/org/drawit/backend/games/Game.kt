5cb65de1cb5a36d92b312b2d501f6e447d7c2af5
Henry Yang <mail@xiaoming.se>
2018-06-25 16:56:09 +0200

    Add long text game description

--- backend/src/main/kotlin/org/drawit/backend/games/Game.kt
+++ backend/src/main/kotlin/org/drawit/backend/games/Game.kt
@@ -1,9 +1,11 @@
 package org.drawit.backend.games
 
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
-data class Game(@field: Id @field: GeneratedValue val Id: Long = 0,
-                val name: String = "")
+data class Game(@field: Id @field: GeneratedValue val id: Long = 0,
+                val name: String = "",
+                @Column(name="description", length = 512) val description: String = "")
--- backend/src/main/kotlin/org/drawit/backend/games/GameRepository.kt
+++ backend/src/main/kotlin/org/drawit/backend/games/GameRepository.kt
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface GameRepository : JpaRepository<Game,Long> {
 
-    fun findByNameContainingIgnoringCase(name: String)
+    fun findByIdOrNameContainingIgnoringCase(id: Long, name: String)
 
 }
