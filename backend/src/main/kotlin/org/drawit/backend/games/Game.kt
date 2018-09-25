4ae69ee161ad7e28bdbefb7fd0588ff02f069b53
Henry Yang <mail@xiaoming.se>
2018-09-25 19:28:43 +0200

    Add relevant data fields to games

--- backend/src/main/kotlin/org/drawit/backend/games/Game.kt
+++ backend/src/main/kotlin/org/drawit/backend/games/Game.kt
@@ -8,4 +8,10 @@ import javax.persistence.Id
 @Entity(name = "games")
 data class Game(@field: Id @field: GeneratedValue val id: Long? = null,
                 val name: String = "",
-                @Column(name="description", length = 512) val description: String = "")
+                @Column(name="description", length = 512) val description: String = "",
+                @Column(name="rule", length = 256) val rules: String = "",
+                @Column(name="player_min") val player_min: Integer = Integer(1),
+                @Column(name="player_max") val player_max: Integer = Integer(1),
+                @Column(name="time") val playTime: String = "",
+                @Column(name="status") val status: String = "",
+                @Column(name="condition") val condition: String = "Complete")
