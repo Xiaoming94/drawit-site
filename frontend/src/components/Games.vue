c3291e47b5ac61d7f708455dca98d33ac4819850
Henry Yang <mail@xiaoming.se>
2018-09-25 20:12:03 +0200

    Show Games as Cards

--- frontend/src/components/Games.vue
+++ frontend/src/components/Games.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="games">
     <h1> Games </h1>
-    <ul>
-        <li v-for="game in games" :key="game.id">
-            <h2>{{ game.name }}</h2>
-            <p>
-              {{ game.description }}
-            </p>
-        </li>
+    <div class="grid-x grid-margin-x">
+        <div class="card cell small=12 medium-4" v-for="game in games" :key="game.id">
+            <div class="card-section">
+              <h2>{{ game.name }}</h2>
+              <h3>Description</h3>
+              <p>
+                {{ game.description }}
+              </p>
+              <h3>Rules</h3>
+              <p>
+                {{ game.rules }}
+              </p>
+              <ul class="inner">
+                <li>
+                  Player Min: {{ game.player_min }}
+                </li>
+                <li>
+                  Player Max: {{ game.player_max }}
+                </li>
+                <li>
+                  Status: {{ game.status }}
+                </li>
+                <li>
+                  Condition: {{ game.condition }}
+                </li>
+              </ul>
+            </div>
+        </div>
         <p>
           Loop exited without problemm
         </p>
-    </ul>
+    </div>
   </div>
 </template>$
 <script>
@@ -36,3 +57,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  text-align: left;
+}
+
+.inner {
+  list-style-type: disc;
+}
+</style>
