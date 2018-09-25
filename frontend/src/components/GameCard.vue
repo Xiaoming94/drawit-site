e3a6880d782212f0c1da4041305a77bd6f4192e9
Henry Yang <mail@xiaoming.se>
2018-09-25 20:54:53 +0200

    Made components of each card

---
+++ frontend/src/components/GameCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-section">
+      <h2>{{ title }}</h2>
+      <h3>Description</h3>
+      <ul class="inner">
+        <li>
+          Playtime: {{ gameTime }}
+        </li>
+        <li>
+          Player Max: {{ playerMax }}
+        </li>
+        <li>
+          Status: {{ playerMin }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    gameTime: String,
+    playerMin: Number,
+    playerMax: Number
+  }
+}
+</script>
--- frontend/src/components/Games.vue
+++ frontend/src/components/Games.vue
@@ -2,45 +2,32 @@
   <div class="games">
     <h1> Games </h1>
     <div class="grid-x grid-margin-x">
-        <div class="card cell small=12 medium-4" v-for="game in games" :key="game.id">
-            <div class="card-section">
-              <h2>{{ game.name }}</h2>
-              <h3>Description</h3>
-              <p>
-                {{ game.description }}
-              </p>
-              <h3>Rules</h3>
-              <p>
-                {{ game.rules }}
-              </p>
-              <ul class="inner">
-                <li>
-                  Player Min: {{ game.player_min }}
-                </li>
-                <li>
-                  Player Max: {{ game.player_max }}
-                </li>
-                <li>
-                  Status: {{ game.status }}
-                </li>
-                <li>
-                  Condition: {{ game.condition }}
-                </li>
-              </ul>
-            </div>
-        </div>
-        <p>
-          Loop exited without problemm
-        </p>
+      <div class="cell small=12 medium-4" v-for="game in games" :key="game.id">
+        <game-card
+          v-bind:title="game.name"
+          v-bind:gameTime="game.playTime"
+          v-bind:playerMin="game.player_min"
+          v-bind:playerMax="game.player_max">
+        </game-card>
+      </div>
     </div>
+    <p>
+      Loop exited without problemm
+    </p>
   </div>
-</template>$
+</template>
+
 <script>
+import GameCard from './GameCard'
+
 export default {
   mixins: [
     // eslint-disable-next-ine
     require('../mixins/foundation')
   ],
+  components: {
+    GameCard
+  },
   name: 'Games',
   data () {
     return {
